4dccfc0193931efcf5c6912ee4449d41efcdc4aa
Fawad Shaikh <fawad@candle.dev>
2023-10-02 15:56:33 -0400

    Add validator and home page

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,4 +1,32 @@
 <script lang="ts">
 </script>
 
-<div class="w-full flex justify-center">Hello!</div>
+<div class="container mx-auto p-6">
+	<div class="text-center">
+		<img src="wick_logo.png" alt="Wick Framework Logo" class="w-96 mx-auto mb-4" />
+		<p class="text-xl mb-4">
+			Unleash the true potential of WebAssembly. Run the same code on the server, client, or the
+			command line.
+		</p>
+	</div>
+
+	<div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+		<div class="bg-gray-100 p-6 rounded-md">
+			<h2 class="text-2xl font-bold mb-2">AI Chatbot</h2>
+			<p class="mb-4">
+				Dramatically reduce server compute costs with Wick. Build and deploy AI chatbots that run
+				efficiently wherever they're hosted.
+			</p>
+			<a href="/aichat" class="text-blue-500 hover:underline">Explore AI Chatbot Example</a>
+		</div>
+
+		<div class="bg-gray-100 p-6 rounded-md">
+			<h2 class="text-2xl font-bold mb-2">Input Validator</h2>
+			<p class="mb-4">
+				Ensure validation consistency across different languages. With Wick, your validation logic
+				is written once and deployed everywhere.
+			</p>
+			<a href="/validator" class="text-blue-500 hover:underline">Explore Input Validator Example</a>
+		</div>
+	</div>
+</div>
---
+++ ui/src/routes/validator/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { Button, Textarea } from 'flowbite-svelte';
+
+	let input = '';
+	let validated = '';
+
+	let inputObj = {
+		name: 'John',
+		age: 'thirty',
+		email: 'john@somewhere.com',
+		ssn: '123-45-6789',
+		ccn: '1234-5678-9012-3456'
+	};
+
+	let sampleInput = JSON.stringify(inputObj, null, 4);
+
+	function validateInput(e: MouseEvent): void {
+		throw new Error('Function not implemented.');
+	}
+</script>
+
+<div class="w-full flex flex-col justify-center items-center">
+	<div class="w-3/5">
+		<div>
+			<Textarea
+				id="input"
+				class="mx-4 resize-none"
+				rows="1"
+				placeholder={sampleInput}
+				bind:value={input}
+				style="height: 12em; max-height: 20em; overflow-y: auto;"
+			/>
+		</div>
+	</div>
+	<div class="flex justify-center mt-2"><Button on:click={validateInput}>Validate</Button></div>
+	<div class="w-3/5 mt-2">
+		<div>
+			<Textarea
+				id="input"
+				class="mx-4 resize-none"
+				rows="1"
+				placeholder="After Validation"
+				bind:value={validated}
+				style="height: 12em; max-height: 20em; overflow-y: auto;"
+			/>
+		</div>
+	</div>
+</div>
